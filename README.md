fa6ff9dadbe9565eaa8c52141f99d34968bb046d
Lee <elcheveremochilero@gmail.com>
2022-10-13 10:36:28 -0500

    change CSS styling including Header component change README.md

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-This is my personal portfolio and showcase of projects I have done.
+### This is my personal *portfolio* and showcase of projects I have done.
 
-The portfolio shall consist of 4 pages: Home, Projects, Contact and Blog. And a header that will be shown in all pages. And there will also be a customised 404 page in case.   
+The portfolio shall consist of 4 pages: **Home**, **Projects**, **Contact** and **Blog**. And a header that will be shown in all pages. And there will also be a customised 404 page in case.   
 
-The page's look shall fit into all devices, including typical desktop(1440px), tablet(768px) and mobile(320px) devices.
+The page's look shall fit into all devices, including typical **desktop(1440px)**, **tablet(768px)** and **mobile(320px)** devices.
 
-Dependencies I will use in this project:
+#### Dependencies I will use in this project:
     
     - react-router-dom for routing
     - react-bootstrap for styling
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,17 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return(
-            <div className="Header" id='h'>
-                
-                <div className="title">Lee's Portfolio</div> 
-
-                <div className="nav">
-                    <Link className="link" to='/'>Home</Link>
-                    <Link className="link" to='/projects'>Projects</Link>
-                    <Link className="link" to='/blog'>Blog</Link>
-                    <Link className="link" to='/contact'>Contact</Link>
-                </div>
-
+            <div>
+                <header className="Header">
+                    <div className="title">Lee's Portfolio</div> 
+                    <div className="nav">
+                        <Link className="link" to='/'>Home</Link>
+                        <Link className="link" to='/projects'>Projects</Link>
+                        <Link className="link" to='/blog'>Blog</Link>
+                        <Link className="link" to='/contact'>Contact</Link>
+                    </div>
+                </header>
             </div>
     )
 };
