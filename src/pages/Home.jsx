b7d2370b2bf5be591c2646ecaccdb820de6d510b
Lee Li KP <84004066+ecmCode@users.noreply.github.com>
2023-01-04 19:13:59 -0600

    lazy loading (#11)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { lazy, Suspense } from 'react'
 
 // normal loading
-import { WelcomePage, Title, Footer, Header, Project} from '../components'
+import { WelcomePage, Title, Footer, Header} from '../components'
 // lazy loading
 const About = lazy(() => import('../components/About'))
 const BlogPost = lazy(() => import('../components/BlogPost'))
 const Skill = lazy(() => import('../components/Skill'))
+const Project = lazy(() => import('../components/Project'))
 
 
 function Home() {
