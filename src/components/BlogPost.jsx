da6a5586355a8a48f6814d24988e7b39f1b9f8b4
Lee <elcheveremochilero@gmail.com>
2023-01-07 18:55:26 -0600

    add filter option for blog

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,29 +1,72 @@
 import { lazy, Suspense, useState, useEffect } from 'react'
 const BlogPostItem = lazy(() => import('./BlogPostItem'))
 
+const BlogFilter = ({posts, filter, setFilter}) => {
+
+    const filterItems = ['all',...new Set(posts.map(p => p.author))]
+
+    const handleClick = (event) => {
+        event.target.id === 'all'
+            ? setFilter(null)
+            : setFilter(event.target.id)
+    }
+
+    const style = {
+        box:"w-1/2 flex flex-col sm:flex-row px-16 lg:px-10  justify-start items-start gap-x-6 gap-y-2",
+        option:"cursor-pointer capitalize px-6 py-2 text-title underline underline-offset-4" 
+    }
+    return(
+        <div className={style.box}>
+            {filterItems.map((f,index) =>{ 
+                return(
+                        <h4
+                        className={style.option}
+                        key={index}
+                        id={f}
+                        onClick={(event) => handleClick(event)}
+                        >
+                            {f}
+                        </h4>
+                )})}
+        </div>
+    )
+}
+
 const BlogPost = () => {
     const [posts,setPosts] = useState([])
-
+    const [filter,setFilter] = useState(null)
+    
     useEffect(() => {
-        setTimeout(() => {
-            import('./db/posts.json').then(p => setPosts(p.default))
-        }, 1000);
-    }, []);
+        import('./db/posts.json')
+        .then(p => {
+            filter
+            ? setPosts(p.default.filter(item => item.author == filter))
+            : setPosts(p.default)
+        })
+    }, [posts, filter]);
     
     return(
-        <div className="p-16 lg:p-10 grid justify-center grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-12 drop-shadow-md">
-            {posts && posts.map(p => (
-                <Suspense fallback={<div></div>}>
-                    <BlogPostItem 
-                    title={p.title}
-                    author={p.author}
-                    date={p.date}
-                    background={p.background}>
-                        {p.content}
-                    </BlogPostItem>
-                </Suspense>
-            ))}
-        </div>
+        <>
+            <BlogFilter 
+            filter={filter}
+            setFilter={setFilter}
+            posts={posts}/>
+
+            <div className="p-16 lg:p-10 grid justify-center grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-12 drop-shadow-md">
+                {posts && posts.map(p => (
+                    <Suspense fallback={<div></div>}>
+                        <BlogPostItem 
+                        title={p.title}
+                        author={p.author}
+                        date={p.date}
+                        background={p.background}>
+                            {p.content}
+                        </BlogPostItem>
+                    </Suspense>
+                ))}
+            </div>
+
+        </>
 
     )
 }
