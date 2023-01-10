16e310861bba1c1aea6b1114ef1b2bfcd584aa29
Lee <elcheveremochilero@gmail.com>
2023-01-10 09:04:10 -0600

    fix error

--- src/components/BlogPostItem.jsx
+++ src/components/BlogPostItem.jsx
@@ -1,9 +1,8 @@
-import fallback from "../assets/fallback_wp.jpg"
 const BlogPostItem = ({title, background, author, date, children}) => {
 
     return(
         <div className="flex flex-col justify-between items-center rounded ring-1 ring-primary animate-fadein-slow">
-            <img src={background || fallback} alt="bg" className="object-cover aspect-video object-center"/>
+            <img src={background} alt="bg" className="object-cover aspect-video object-center"/>
             <div className="p-6 h-full w-full flex flex-col items-center justify-between">
                 <h4 className="text-center w-4/5 my-4">{title}</h4>
                 <div className="flex justify-between w-full p-2">
