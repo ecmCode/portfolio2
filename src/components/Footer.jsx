da95ffa2472746f0a5bc4de60a928b8bb75828e3
Lee <elcheveremochilero@gmail.com>
2023-02-03 16:02:29 -0600

    map jsx elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,19 +22,30 @@ const ContactInfo = () => {
     )
 }
 
-const Icons = () => {
+const Icon = () => {
+
+    const icons = [
+        {className:"fa-brands fa-github", url: "https://github.com/"},
+        {className:"fa-brands fa-instagram", url: "https://www.instagram.com/"},
+        {className:"fa-brands fa-youtube", url: "https://www.youtube.com/"},
+        {className:"fa-brands fa-facebook", url: "https://www.facebook.com/"},
+    ]
+
     return(
         <div className="grid grid-cols-4 lg:grid-cols-2 text-4xl gap-6">
-            <i className="fa-brands fa-github"/>
-            <i className="fa-brands fa-instagram" />
-            <i className="fa-brands fa-youtube" />
-            <i className="fa-brands fa-facebook" />
+           {icons.map(icon => 
+                <a href={icon.url} target="_blank">
+                    <i className={icon.className}/>
+                </a>
+            )}
         </div>
     )
 }
 
 const GoogleMap = () => {
+
     const src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d23704002.510822084!2d-70.01390205864136!3d21.303065452748353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen-US!2smx!4v1669435444432!5m2!1sen-US!2smx" 
+
     return(
         <iframe 
             src={src}
@@ -73,7 +84,7 @@ const Footer = () => {
                     <div className={style.sidebar}>
                         <GoogleMap/>
                         <div className={style.sidebarInfo}>
-                            <Icons/>
+                            <Icon/>
                             <ContactInfo/>
                         </div>
                     </div>
